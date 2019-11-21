bed4fc2f3d8b25ab00834fc980d4d9458dad2a8f
Zhizhen Wang <dengluwzz@gmail.com>
2019-11-21 01:24:43 +0100

    Add legend and fix bugs
    - add color distance relation legend
    - click on the station will reset color

--- cycle_crash/static/main.js
+++ cycle_crash/static/main.js
@@ -1,15 +1,16 @@
+myToken = 'pk.eyJ1IjoiemhpemhlbmdpdCIsImEiOiJjazM3c3g4aGkwMjVxM2RvdnhpcWxxYTY4In0.jDvET27hbKAGDRSB2r1xIw';
 //map center point
 BASECOORDS = [40.7057, -73.9205];
 //crash marker default distance for color
-DEFAULT_DIST = 9000
+DEFAULT_DIST = -1
 //crash layerGroup which store markers, index by borough
 var crashes = [];
 var crashMarkers = []
 //crash distance from the bike station
 var distanceCrash = [];
 //bikestation layerGroup
-var stations = L.featureGroup()
-var boroughName = ["Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"]
+var stations = L.featureGroup();
+var boroughName = ["Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"];
 
 //marker icon class
 var AwesomeIcon = L.DivIcon.extend({
@@ -23,11 +24,11 @@ var AwesomeIcon = L.DivIcon.extend({
 var stationIcon = new AwesomeIcon({
   html: `<span class='fa-stack fa-2x'>
           <i class='fas fa-map-marker fa-stack-2x' style='color: #192486'></i>
-          <i class='fas fa-biking fa-stack-1x fa-inverse' style='color: #39A2E1 font-size: .4em;'></i>
+          <i class='fas fa-biking fa-stack-1x' style='font-size: .7em;'></i>
         </span>`
 })
 
-var crashIcon = function(color) {
+var crashIcon = function (color) {
   return new AwesomeIcon({
     html: `<span class='fa-stack fa-2x'>
           <i class='fas fa-map-marker fa-stack-2x' style='color:` +
@@ -39,30 +40,35 @@ var crashIcon = function(color) {
 }
 
 // crash color icon function
-var logistic = function(x) {
+var logistic = function (x) {
   x = x / 1000;
   return 100 * (1.0 * (1 / (1 + Math.exp(-0.1 * x))) - 0.2);
 };
-var redDynamic = function(x) {
-  return "hsl(8, 76%," + logistic(x) + "%)"
+var step = function (x) {
+  return x > 20000 ? 80 :
+    x > 10000 ? 50 :
+      x > 5000 ? 40 :
+        20;
+}
+var redDynamic = function (x) {
+  return x > 0 ? "hsl(8, 76%," + step(x) + "%)" : "#eab35a"
 };
 
 function makeMap() {
   var TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   var MB_ATTR = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+
+  street = L.tileLayer(TILE_URL, { attribution: MB_ATTR })
   mymap = L.map('imap', {
-    layers: Object.values(crashes)
+    layers: Object.values(crashes).concat(street)
   }).setView(BASECOORDS, 11);
-  L.tileLayer(TILE_URL, {
-    attribution: MB_ATTR
-  }).addTo(mymap);
 }
 
-var updateCrashMarkers = function(target) {
+function updateCrashMarkers(target) {
   if (target) {
     var stationLatLng = target.getLatLng();
   }
-  crashMarkers.forEach(function(crashMarker) {
+  crashMarkers.forEach(function (crashMarker) {
     if (target) {
       var radius = stationLatLng.distanceTo(crashMarker.getLatLng());
       distanceCrash[crashMarker._leaflet_id] = _round(radius, 1);
@@ -72,7 +78,7 @@ var updateCrashMarkers = function(target) {
       distanceCrash = [];
     }
   });
-};
+}
 
 function stationClickEvent(e) {
   this._bringToFront()
@@ -82,11 +88,11 @@ function stationClickEvent(e) {
 }
 
 function renderStation() {
-  $.getJSON("/stations", function(obj) {
-    obj.map(function(arr) {
+  $.getJSON("/stations", function (obj) {
+    obj.map(function (arr) {
       return L.marker([arr['latitude'], arr['longitude']], {
-          icon: stationIcon
-        })
+        icon: stationIcon
+      })
         .bindPopup('Station Name: ' + arr['stationName'] + '</br>Status: ' + arr['statusValue'] +
           '</br>Available Docks: ' + arr['availableDocks'] +
           '</br>Available Bikes: ' + arr['availableBikes'])
@@ -102,18 +108,18 @@ function showDistance() {
 }
 var crashEvent = {
   mouseover: showDistance,
-  click: function() {
+  click: function () {
     this._bringToFront()
   }
 }
 
 function renderCrash(borough) {
   crashes[borough] = L.featureGroup();
-  $.getJSON("/crash/" + borough, function(obj) {
-    obj.map(function(arr) {
+  $.getJSON("/crash/" + borough, function (obj) {
+    obj.map(function (arr) {
       var markerTmp = L.marker([arr['LATITUDE'], arr['LONGITUDE']], {
-          icon: crashIcon(redDynamic(DEFAULT_DIST))
-        })
+        icon: crashIcon(redDynamic(DEFAULT_DIST))
+      })
         .bindPopup('Collision ID: ' + arr['COLLISION_ID'] + '</br>Date: ' +
           arr['ACCIDENT DATE'] + '</br>Time: ' + arr['ACCIDENT TIME'] + '</br>Injured: ' +
           arr['NUMBER OF CYCLIST INJURED'] + '</br>Killed: ' + arr['NUMBER OF CYCLIST KILLED'])
@@ -128,58 +134,83 @@ function renderCrash(borough) {
 var distanceInfo = L.control({
   position: 'bottomright'
 });
-distanceInfo.onAdd = function(map) {
-  this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+distanceInfo.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info legend'); // create a div with a class "info"
   this.update();
   return this._div;
 }
 // method that we will use to update the control based on feature properties passed
-distanceInfo.update = function(dis) {
-  this._div.innerHTML = '<h4>Distance to Bike Station</h4>' + (dis ?
-    +dis + ' m' :
-    'Click one station, and hover over a crash location');
+distanceInfo.update = function (dis) {
+  grades = [0, 5, 10, 20]
+  legendHTML = '<h4>Distance to Bike Station</h4>'
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+    legendHTML +=
+      '<i style="background:' + redDynamic(grades[i] * 1000 + 1) + '"></i> ' +
+      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] : '+') + 'km<br>';
+  }
+  this._div.innerHTML = legendHTML + '<p>' + (dis ? 'Current distance:<br>' + dis + ' m' :
+    'Click one station, and hover over a crash location') + '</p>';
 };
 
+// function addOverlay() {
+//   var overlayMaps = [{
+//     group: "Crash Location",
+//     collapsed: true,
+//     layers: boroughName.map(function (id) {
+//       return {
+//         name: id,
+//         layer: crashes[id]
+//       }
+//     })
+//   },
+//   {
+//     group: "Bike Station",
+//     layers: [{
+//       name: "Citibike",
+//       layer: stations
+//     }]
+//   }
+//   ]
+
+//   pl = L.control.panelLayers(null, overlayMaps, {
+//     collapsed: false,
+//     //        collapsibleGroups: true,
+//     compact: true,
+//   }).addTo(mymap);
+// }
+
 function addOverlay() {
-  var overlayMaps = [{
-      group: "Crash Location",
-      collapsed: true,
-      layers: boroughName.map(function(id) {
-        return {
-          name: id,
-          layer: crashes[id]
-        }
-      })
+  var groupedOverlays = {
+    'Crash Location': crashes,
+    "Bike Station": {
+      "Citibike": stations
     },
-    {
-      group: "Bike Station",
-      layers: [{
-        name: "Citibike",
-        layer: stations
-      }]
+    "Borough": {
+      "On/Off": L.geoJson(boroughData, { style: style })
     }
-  ]
+  };
 
-  pl = L.control.panelLayers(null, overlayMaps, {
-    collapsed: false,
-    //        collapsibleGroups: true,
-    compact: true,
-  }).addTo(mymap);
+  var baseMaps = {
+    "Street": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token='+myToken),
+    "Light View": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}?access_token='+myToken),
+    "OpenStreetMap": street,
+  };
+  L.control.groupedLayers(baseMaps, groupedOverlays, { collapsed: true }).addTo(mymap);
 }
-
 
 // Below are Draw layout
 // Truncate value based on number of decimals
-var _round = function(num, len) {
+var _round = function (num, len) {
   return Math.round(num * (Math.pow(10, len))) / (Math.pow(10, len));
 };
 // Helper method to format LatLng object (x.xxxxxx, y.yyyyyy)
-var strLatLng = function(latlng) {
+var strLatLng = function (latlng) {
   return "(" + _round(latlng.lat, 6) + ", " + _round(latlng.lng, 6) + ")";
 };
 // Generate popup content based on layer type
 // - Returns HTML string, or null if unknown object
-var getPopupContent = function(layer) {
+var getPopupContent = function (layer) {
   if (layer instanceof L.Circle) {
     var center = layer.getLatLng(),
       radius = layer.getRadius();
@@ -233,7 +264,7 @@ function addDrawPlugin() {
     }
   });
   mymap.addControl(drawControl);
-  mymap.on(L.Draw.Event.CREATED, function(e) {
+  mymap.on(L.Draw.Event.CREATED, function (e) {
     var layer = e.layer;
     var content = getPopupContent(layer);
     if (content !== null) {
@@ -242,10 +273,10 @@ function addDrawPlugin() {
     drawnItems.addLayer(layer);
   });
   // Object(s) edited - update popups
-  mymap.on(L.Draw.Event.EDITED, function(e) {
+  mymap.on(L.Draw.Event.EDITED, function (e) {
     var layers = e.layers,
       content = null;
-    layers.eachLayer(function(layer) {
+    layers.eachLayer(function (layer) {
       content = getPopupContent(layer);
       if (content !== null) {
         layer.setPopupContent(content);
@@ -265,23 +296,28 @@ function style(feature) {
   };
 }
 
-$(function() {
+$(function () {
+
   for (var index in boroughName) {
     renderCrash(boroughName[index]);
   }
   renderStation();
 
   makeMap();
-  //todo seperate geojson to layerGroup, or use different tile
-  //    L.geoJson(boroughData, {style: style}).addTo(mymap);
 
   addOverlay()
   addDrawPlugin()
 
   distanceInfo.addTo(mymap)
-  mymap.on('click', function(e) {
+  mymap.on('click', function (e) {
     updateCrashMarkers();
-    stations.addTo(mymap)
-    distanceInfo.update()
+    stations.addTo(mymap);
+    distanceInfo.update();
   });
+  mymap.on('overlayadd', function (e) {
+    if (e.name === 'Citibike') {
+      updateCrashMarkers();
+      distanceInfo.update();
+    }
+  })
 })
